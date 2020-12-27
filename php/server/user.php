9e5cad3da383ef65a225e690e50533e3e2af5e39
Jannik66 <ja.noetzli@gmail.com>
2020-12-27 15:19:52 +0100

    Add php sql connection and createUser & loginUser function

---
+++ php/server/user.php
@@ -0,0 +1,167 @@
+<?php
+function createUser($user)
+{
+    // Database connection
+    include('sqlConnection.php');
+
+    $errorString = '';
+
+    $letterOnly = "/^[a-zA-Z]+$/i";
+    $passwordPattern = "/^[ -~]+$/";
+
+    // Validation of userid
+    // not empty
+    // length: 3-20
+    // only letters($letterOnly regex)
+    $trimmedUserId = trim($user["userid"]);
+    if (
+        !empty($trimmedUserId) &&
+        strlen($trimmedUserId) >= 3 && strlen($trimmedUserId) <= 20 &&
+        preg_match($letterOnly, $trimmedUserId)
+    ) {
+        $userid = htmlentities($trimmedUserId);
+    } else {
+        $errorString .= "Invalid userid\n";
+    }
+
+    // Validation of name
+    // not empty
+    // length: 3-45
+    $trimmedName = trim($user["name"]);
+    if (
+        !empty($trimmedName) &&
+        strlen($trimmedName) >= 3 && strlen($trimmedName) <= 45
+    ) {
+        $name = htmlentities($trimmedName);
+    } else {
+        $errorString .= "Invalid name\n";
+    }
+
+    // Validation of email
+    // not empty
+    // length: 1-100
+    $trimmedEmail = trim($user["email"]);
+    if (
+        !empty($trimmedEmail) &&
+        strlen($trimmedEmail) >= 1 && strlen($trimmedEmail) <= 100
+    ) {
+        $email = htmlentities($trimmedEmail);
+    } else {
+        $errorString .= "Invalid email\n";
+    }
+
+    // Validation of password
+    // not empty
+    // length: 8-255
+    if (
+        !empty($user["password"]) &&
+        strlen($user["password"]) >= 8 && strlen($user["password"]) <= 255 &&
+        preg_match($passwordPattern, $user["password"])
+    ) {
+        $password = htmlentities($user["password"]);
+    } else {
+        $errorString .= "Invalid password\n";
+    }
+
+    if (empty($errorString)) {
+        $query = "SELECT * FROM user where userid=? limit 1";
+        $stmt = $mysqli->prepare($query);
+
+        $stmt->bind_param("s", $userid);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $firstRow = $result->fetch_row();
+
+        if (isset($firstRow)) {
+            $errorString .= "User already exists\n";
+        }
+    }
+
+    if (empty($errorString)) {
+        // INPUT Query erstellen
+        $query = "INSERT INTO user(userid, name, email, password) VALUES (?,?,?,?)";
+        // Query vorbereiten mit prepare();
+        $stmt = $mysqli->prepare($query);
+        if ($stmt === false) {
+            $errorString .= 'prepare() failed ' . $mysqli->errorString . '<br />';
+        }
+        // Parameter an Query binden mit bind_param();
+        if (!$stmt->bind_param('ssss', $userid, $name, $email, password_hash($password, PASSWORD_BCRYPT))) {
+            $errorString .= 'bind_param() failed ' . $mysqli->errorString . '<br />';
+        }
+        // query ausführen mit execute();
+        if (!$stmt->execute()) {
+            $errorString .= 'execute() failed ' . $mysqli->errorString . '<br />';
+        }
+        // Verbindung schliessen
+        $stmt->close();
+    }
+
+    // TODO handle error string and redirect to other site
+    echo "ErrorString:";
+    echo $errorString;
+}
+
+function loginUser($loginUser)
+{
+    // Database connection
+    include('sqlConnection.php');
+
+    $errorString = '';
+
+    // validate userid
+    if (isset($loginUser['userid'])) {
+        //trim
+        $userid = trim($loginUser['userid']);
+    } else {
+        $errorString .= "Please provide a userid";
+    }
+
+    // validate password
+    if (isset($loginUser['password'])) {
+        //trim
+        $password = trim($loginUser['password']);
+    } else {
+        $errorString .= "Please provide a password";
+    }
+
+    if (empty($errorString)) {
+
+        $query = "SELECT * FROM user where userid=? limit 1";
+        $stmt = $mysqli->prepare($query);
+
+        $stmt->bind_param("s", $userid);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        $firstRow = $result->fetch_assoc();
+
+        if (!isset($firstRow)) {
+            $errorString .= "Invalid userid or password";
+        }
+
+        if (empty($errorString)) {
+            if (password_verify($password, $firstRow['password'])) {
+                session_start();
+                $_SESSION['loggedin'] = true;
+                $_SESSION['userid'] = $firstRow['userid'];
+                $_SESSION['name'] = $firstRow['name'];
+            }
+
+            if (!isset($_SESSION['loggedin']) or isset($_SESSION['loggedin']) and !$_SESSION['loggedin']) {
+                $errorString .= "Invalid userid or password";
+            }
+        }
+
+        $result->free();
+    }
+
+    // TODO handle error string and redirect to other site
+    echo "ErrorString:";
+    echo $errorString;
+}
